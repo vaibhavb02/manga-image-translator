f61caa610a26758ef401a8e91f815e5bc74f6dea
vaibhavb02 <33981696+vaibhavb02@users.noreply.github.com>
2023-06-21 11:01:43 +0530

    Update __main__.py

--- manga_translator/__main__.py
+++ manga_translator/__main__.py
@@ -23,40 +23,39 @@ from .utils import (
 
 async def dispatch(args: Namespace):
     args_dict = vars(args)
-
-    logger.info(f'Running in {args.mode} mode')
-
-    if args.mode in ('demo', 'batch'):
-        if not args.input:
-            raise Exception('No input image was supplied. Use -i <image_path>')
-        translator = MangaTranslator(args_dict)
-        if args.mode == 'demo':
-            if len(args.input) != 1 or not os.path.isfile(args.input[0]):
-                raise FileNotFoundError(f'Invalid single image file path for demo mode: "{" ".join(args.input)}". Use `-m batch`.')
-            dest = os.path.join(BASE_PATH, 'result/final.png')
-            args.overwrite = True # Do overwrite result/final.png file
-            await translator.translate_path(args.input[0], dest, args_dict)
-        else: # batch
-            dest = args.dest
-            args.overwrite = False
-            for path in natural_sort(args.input):
-                await translator.translate_path(path, dest, args_dict)
-
-    elif args.mode == 'web':
-        from .server.web_main import dispatch
-        await dispatch(args.host, args.port, translation_params=args_dict)
-
-    elif args.mode == 'web_client':
-        translator = MangaTranslatorWeb(args_dict)
-        await translator.listen(args_dict)
-
-    elif args.mode == 'ws':
-        translator = MangaTranslatorWS(args_dict)
-        await translator.listen(args_dict)
-
-    elif args.mode == 'api':
-        translator = MangaTranslatorAPI(args_dict)
-        await translator.listen(args_dict)
+    logger = get_logger(args.mode)
+        logger.info(f'Running in {args.mode} mode')
+        if args.mode in ('demo', 'batch'):
+            if not args.input:
+                raise Exception('No input image was supplied. Use -i <image_path>')
+            translator = MangaTranslator(args_dict)
+            if args.mode == 'demo':
+                if len(args.input) != 1 or not os.path.isfile(args.input[0]):
+                    raise FileNotFoundError(f'Invalid single image file path for demo mode: "{" ".join(args.input)}". Use `-m batch`.')
+                dest = os.path.join(BASE_PATH, 'result/final.png')
+                args.overwrite = True  # Do overwrite result/final.png file
+                await translator.translate_path(args.input[0], dest, args_dict)
+            else:  # batch
+                dest = args.dest
+                args.overwrite = False
+                for path in natural_sort(args.input):
+                    await translator.translate_path(path, dest, args_dict)
+        elif args.mode == 'web':
+            from .server.web_main import dispatch
+            loop = asyncio.get_event_loop()
+            await loop.run_until_complete(dispatch(args.host, args.port, translation_params=args_dict))
+        elif args.mode == 'web_client':
+            translator = MangaTranslatorWeb(args_dict)
+            loop = asyncio.get_event_loop()
+            await loop.run_until_complete(translator.listen(args_dict))
+        elif args.mode == 'ws':
+            translator = MangaTranslatorWS(args_dict)
+            loop = asyncio.get_event_loop()
+            await loop.run_until_complete(translator.listen(args_dict))
+        elif args.mode == 'api':
+            translator = MangaTranslatorAPI(args_dict)
+            loop = asyncio.get_event_loop()
+            await loop.run_until_complete(translator.listen(args_dict))
 
 if __name__ == '__main__':
     args = None
